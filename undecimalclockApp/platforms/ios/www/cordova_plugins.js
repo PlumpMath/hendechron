67f6dc671dcc7fe98efee73e19fc0c1b4fe409cc
Dan Taeyoung <provolot@gmail.com>
2014-08-09 01:12:02 -0400

    removed testflight plugin

--- undecimalclockApp/platforms/ios/www/cordova_plugins.js
+++ undecimalclockApp/platforms/ios/www/cordova_plugins.js
@@ -13,21 +13,13 @@ module.exports = [
         "clobbers": [
             "device"
         ]
-    },
-    {
-        "file": "plugins/com.testflightapp.cordova-plugin/www/testflight.js",
-        "id": "com.testflightapp.cordova-plugin.TestFlight",
-        "clobbers": [
-            "TestFlight"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "com.ionic.keyboard": "1.0.2",
-    "org.apache.cordova.device": "0.2.10",
-    "com.testflightapp.cordova-plugin": "3.1.0"
+    "org.apache.cordova.device": "0.2.10"
 }
 // BOTTOM OF METADATA
 });
