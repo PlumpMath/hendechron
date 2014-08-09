a86666cb71e0b65c154e0883ff2419f7b6846770
Dan Taeyoung <provolot@gmail.com>
2014-08-09 04:18:02 -0400

    regenerated ios dir

--- platforms/ios/Hendechron/Classes/MainViewController.h
+++ platforms/ios/Hendechron/Classes/MainViewController.h
@@ -19,7 +19,7 @@
 
 //
 //  MainViewController.h
-//  undecimalclock
+//  Hendechron
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
